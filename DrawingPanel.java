1301f34eb2821ca493af63bb89fc3ba65b1f5d66
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2020-04-21 01:39:16 -0700

    Create DrawingPanel.java

---
+++ DrawingPanel.java
@@ -0,0 +1,35 @@
+/*
+ * Author: Espoir Makiese 
+ * Date: 4/20/20
+ * Assignment 1 ITC 155
+ */
+
+public class Assignment1 
+{
+	public static void main(String[] args) 
+	{
+		//Creates a yellow panel
+		DrawingPanel panel = new DrawingPanel(280, 200);
+		panel.setBackground(Color.YELLOW);
+		
+		//Allows ability to draw shapes
+		Graphics g = panel.getGraphics();
+
+		//Blue circle left
+		g.setColor(Color.BLUE);
+		g.fillOval(60, 45, 50, 50);
+		
+		//Blue circle right
+		g.setColor(Color.BLUE);
+		g.fillOval(160, 45, 50, 50);
+		
+		//Red square
+		g.setColor(Color.RED);
+		g.fillRect(85, 70, 100, 100);
+
+		//Line in the middle of square 
+		g.setColor(Color.BLACK);
+		g.drawLine(85, 125, 185, 125);
+	}
+
+}
